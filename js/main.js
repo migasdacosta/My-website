224185add125519649c811b89dfa6e7cb3821169
migasdacosta <mikejolas@Miguel-Furtado-costas-MacBook-Pro.local>
2013-08-11 18:00:13 +0100

    tentativa de menu collapsble

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-
+$(document).ready(function() {
+    // Append class to contact
+    $('.wrapper aside div.aux section.logo a').click(function() {
+        $(".wrapper aside").removeClass("menu-open");
+        $(".wrapper aside").addClass("menu-open");
+    });
+});
