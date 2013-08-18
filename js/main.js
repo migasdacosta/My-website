a7ed1ca7e777b20eda93f96f67cdec9a4c646dad
migasdacosta <mikejolas@Miguel-Furtado-costas-MacBook-Pro.local>
2013-08-18 23:42:05 +0100

    menu collapsable working, still some work to do

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-    // Append class to contact
-    $('.wrapper aside div.aux section.logo a').click(function() {
-        $(".wrapper aside").removeClass("menu-open");
-        $(".wrapper aside").addClass("menu-open");
-    });
+    
+    
+    $('i').click(function() {
+    $('aside').slideToggle('fast');
+    return false;
+	});
+
+    
 });
