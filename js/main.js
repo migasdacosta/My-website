e5ac704cade9990d610fb80edb9206280cf3d214
migasdacosta <mikejolas@Miguel-Furtado-costas-MacBook-Pro.local>
2013-08-22 23:12:17 +0100

    dribbble plugin index

--- js/main.js
+++ js/main.js
@@ -17,11 +17,11 @@ $(document).ready(function () {
 	    var html = [];
 	
 	    $.each(playerShots.shots, function (i, shot) {
-	        html.push('<li><a href="' + shot.url + '">');
+	        html.push('<li><a href="#">');
 	        html.push('<img src="' + shot.image_teaser_url + '" ');
 	        html.push('alt="' + shot.title + '"></a></li>');
 	    });
 	
 	    $('#portfolio').html(html.join(''));
-	}, {page: 1, per_page: 12});
+	}, {page: 1, per_page: 50});
 });
